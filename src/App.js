abade4fef7cdf510515ec8a1274792281a64b966
Ignat Tokar <example@example.com>
2022-06-30 14:59:25 +0300

    added start style for first 4 pages

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
 import './App.css';
+import mainBackgroundImage from './assets/main-background.jpeg';
+import menuBackgroundImage from './assets/menu-background.jpeg';
 
 function App() {
   return (
     <div>
       <div className="Main-background"
         style={{
-          backgroundImage: "url('https://www.linguahouse.com/linguafiles/md5/d01dfa8621f83289155a3be0970fb0cb')"
+          backgroundImage: `url('${mainBackgroundImage}')`
         }}
       >
         <div className="Menu">
-          <span>LOGO</span>
+          <span id="logo">LOGO</span>
           <ul>
             <li>O NAS</li>
             <li>MENU</li>
             <li>AKCE</li>
-            <li>SALONEK</li>
             <li>KONTAKT</li>
           </ul>
         </div>
-        <span>PIVO KLENOT</span>
-      </div>
-      <div className="Rozvoz-section">
-        <h2>ROZVOZ JÍDLA A PIVA</h2>
-        <p>Rádi vám dovezeme jídlo a pivo v Hradci Králové
-
-          (rozvoz zdarma).
-
-          Prohlédněte si stálý jídelní lístek a denní nabídku.
-
-          Pro objednávku volejte 734 245 400 nebo využijte:</p>
-        <span>On-line objednávku</span>
+        <span id="title">PIVO KLENOT</span>
       </div>
       <div className="O-nas">
         <h2>O RESTAURACI</h2>
-        <p>Stylová restaurace s vášní pro gastronomii a láskou k Hradci přímo v srdci města na Velkém náměstí.
+        <div className="p-block">
+          <p>Stylová restaurace s vášní pro gastronomii a láskou k Hradci přímo v srdci města na Velkém náměstí.
 
-          Přijďte k nám vyzkoušet domácí recepty a poctivou práci našich kuchařů pod vedením šéfkuchaře Michaela Seidla.
+          </p><p>Přijďte k nám vyzkoušet domácí recepty a poctivou práci našich kuchařů pod vedením šéfkuchaře Michaela Seidla.
 
-          Celý tým se na vás moc těší.</p>
-        <span>ČÍST DÁLE</span>
+          </p><p>Celý tým se na vás moc těší.</p>
+        </div>
+        <span id="cist-dale">ČÍST DÁLE</span>
       </div>
       <div className="Menu-today"
         style={{
-          backgroundImage: "url('https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80')"
-        }}        
+          backgroundImage: `url('${menuBackgroundImage}')`
+        }}
       >
-        <h2>Denni menu</h2>
-        <p>POLEDNÍ MENU / POLÉVKA + HLAVNÍ CHOD
-          POLÉVKA : Minestrone s kuskusem - v ceně menu,-
-          MENU 1 : VEGE tortilla pněná sezonní restovanou zeleninou a zapečená cheddarem, domácí guacamole - 155,-
-          MENU 2 : Spaghetti bolognese / čerstvé domácí těstoviny s hovězím ragú (140g) s kořenovou zeleninou a červeným vínem, sypané parmesanem - 165,-
-          MENU 3 : Grilovaný steak z vepřové kotlety US Duroc (160g) s krémovou pepřovou omáčkou, bramborovým gratinem a máslovou brokolicí - 170,-
-          SPECIÁL : Rice bowl s krevetami v tempuře (80g), jasmínová rýže, nakládaná okurka, karotka, teriyaki, sezam - 275,-
-          DEZERT RESTAURACE : Jablečný závin s vanilkovým krémem - 40,-
-          DEZERT ROZVOZ : Cheesecake se slaným karamelem a arašídy - 40,-</p>
+        <p id="menu-title">Denni menu na 30.6.2022</p>
+        <hr />
+        <p id="menu-row">POLEDNÍ MENU / POLÉVKA + HLAVNÍ CHOD</p>
+        <hr />
+        <p id="menu-row">POLÉVKA : Minestrone s kuskusem - v ceně menu,-</p>
+        <hr />
+        <p id="menu-row">MENU 1 : VEGE tortilla pněná sezonní restovanou zeleninou a zapečená cheddarem, domácí guacamole - 155,-</p>
+        <hr />
+        <p id="menu-row">MENU 2 : Spaghetti bolognese / čerstvé domácí těstoviny s hovězím ragú (140g) s kořenovou zeleninou a červeným vínem, sypané parmesanem - 165,-</p>
+        <hr />
+        <p id="menu-row">MENU 3 : Grilovaný steak z vepřové kotlety US Duroc (160g) s krémovou pepřovou omáčkou, hramborovým gratinem a máslovou hrokolicí - 170,-</p>
+        <hr />
+        <p id="menu-row">SPECIÁL : Rice bowl s krevetami v tempuře (80g), jasmínová rýže, nakládaná okurka, karotka, teriyaki, sezam - 275,-</p>
+        <hr />
+        <p id="menu-row">DEZERT RESTAURACE : Jablečný závin s vanilkovým krémem - 40,-</p>
+        <hr />
+        <p id="menu-row">DEZERT ROZVOZ : Cheesecake se slaným karamelem a arašídy - 40,-</p>
       </div>
       <div className="Menu-all">
         <div className="Vertical-menu-wrapper">
           <div className="Vertical-menu"
             style={{
               backgroundImage: "url('https://restaurace.pivovarskedomy.cz/files/thumbnails/setting/0/tb0st/denni-menu-karta-bg_700x1000_1_0.jpg')"
-            }}           
+            }}
           >
             <h2>DENNI MENU</h2>
             <a href="#">Prohlédnout</a>
@@ -68,7 +68,7 @@ function App() {
           <div className="Vertical-menu"
             style={{
               backgroundImage: "url('https://restaurace.pivovarskedomy.cz/files/thumbnails/setting/0/tb0st/denni-menu-karta-bg_700x1000_1_0.jpg')"
-            }}            
+            }}
           >
             <h2>DENNI MENU</h2>
             <a href="#">Prohlédnout</a>
@@ -76,7 +76,7 @@ function App() {
           <div className="Vertical-menu"
             style={{
               backgroundImage: "url('https://restaurace.pivovarskedomy.cz/files/thumbnails/setting/0/tb0st/denni-menu-karta-bg_700x1000_1_0.jpg')"
-            }}            
+            }}
           >
             <h2>DENNI MENU</h2>
             <a href="#">Prohlédnout</a>
@@ -86,12 +86,16 @@ function App() {
           <div className="Horizontal-menu"
             style={{
               backgroundImage: "url('https://restaurace.pivovarskedomy.cz/files/thumbnails/setting/0/4vsrz/napojovy-listek-karta-bg_1080x460_1_0.png')"
-            }} 
->
+            }}
+          >
             <h2>Napojovy listek</h2>
             <a href="#">Prohlédnout</a>
           </div>
-          <div className="Horizontal-menu">
+          <div className="Horizontal-menu"
+            style={{
+              backgroundImage: "url('https://restaurace.pivovarskedomy.cz/files/thumbnails/setting/0/4vsrz/napojovy-listek-karta-bg_1080x460_1_0.png')"
+            }}
+          >
             <h2>Napojovy listek</h2>
             <a href="#">Prohlédnout</a>
           </div>
@@ -103,15 +107,15 @@ function App() {
         <h3>Nenechte si ujít nejbližší akce</h3>
         <div className="Akce-block">
           <img src="https://restaurace.pivovarskedomy.cz/files/thumbnails/image_contents/0/uqgq1/coolinarka-pd4_1068x710_1_0.jpg" />
-          <span>30.-3.4.2022</span>
-          <span>ASIE</span>
+          <span id="date">30.-3.4.2022</span>
+          <span id="akce-title">ASIE</span>
         </div>
         <div className="Akce-block">
           <img src="https://restaurace.pivovarskedomy.cz/files/thumbnails/image_contents/0/uqgq1/coolinarka-pd4_1068x710_1_0.jpg" />
           <span>30.-3.4.2022</span>
           <span>ASIE</span>
         </div>
-        <span>Dalsi</span>
+        <span id="akce-button">Dalsi</span>
       </div>
 
       <div className="Footer">
